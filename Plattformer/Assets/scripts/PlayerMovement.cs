790a7efeca19df4aa589a485d95754271062dca1
Philip Nguyen <yetrexgamer@gmail.com>
2025-11-10 17:12:00 +0100

    Some movement achieved
    Some movement achieved

---
+++ Plattformer/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    private Vector2 _moveDirection;
+    public InputActionReference move;
+
+    public void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+}
