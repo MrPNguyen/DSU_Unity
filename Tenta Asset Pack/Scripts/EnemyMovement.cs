127169ea2be699ba481f8f0a649e2802a59b9ef2
Philip Nguyen <yetrexgamer@gmail.com>
2025-12-23 12:50:41 +0100

    Uppdaterad Asset Pack

---
+++ Tenta Asset Pack/Scripts/EnemyMovement.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+
+    [SerializeField] private float enemyMoveSpeed = 3f;
+    public float WaitBeforeWalking = 2f;
+
+    private float EnemyDirection;
+    private bool canMove = false;
+
+    private void Start()
+    {
+        EnemyDirection = 1f;
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(DelayMovementCoroutine());
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Vector2 newPos = rb.position + new Vector2(enemyMoveSpeed * EnemyDirection, 0) * Time.fixedDeltaTime;
+            rb.MovePosition(newPos);
+        }
+    }
+
+    public void StartMovingAfterRespawn(float riseLength)
+    {
+        canMove = false;
+        anim.SetBool("isWalking", false);
+        StartCoroutine(DelayMovementCoroutine());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PointA"))
+        {
+            EnemyDirection = 1;
+            sr.flipX = true;
+        }
+
+        else if (other.gameObject.CompareTag("PointB"))
+        {
+            EnemyDirection = -1;
+            sr.flipX = false;
+        }
+    }
+
+    public void StartMovementAfterDelay()
+    {
+        StartCoroutine(DelayMovementAfterRespawnCoroutine());
+    }
+
+    private IEnumerator DelayMovementAfterRespawnCoroutine()
+    {
+        canMove = false;
+        anim.SetBool("isWalking", false);
+
+        yield return new WaitForSeconds(WaitBeforeWalking);
+
+        yield return new WaitForSeconds(WaitBeforeWalking);
+        canMove = true;
+        anim.SetBool("isWalking", true);
+    }
+
+    private IEnumerator DelayMovementCoroutine()
+    {
+        canMove = false;
+        anim.SetBool("isWalking", false);
+
+        yield return new WaitForSeconds(WaitBeforeWalking); // initial wait
+        canMove = true;
+        anim.SetBool("isWalking", true);
+    }
+}
---
+++ Tenta Asset Pack/Scripts/Player/PlayerSFXManager.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerSFXManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("SFX")]
+    [SerializeField] private AudioClip jumpClip;
+    [SerializeField] private AudioClip hardLandClip;
+    [SerializeField] private AudioClip dashClip;
+    [SerializeField] private AudioClip hurtClip;
+    private AudioSource audioSource;
+    [SerializeField] private AudioSource OneShotSource;
+    
+    [Header("Walking")]
+    [SerializeField] private List<AudioClip> walkingClips;
+    private PlayerMovement playerMove;
+    private float walkingTimer;
+    [SerializeField] private float walkingInterval = 0.10f;
+    [SerializeField] private float runningMultiplier = 0.6f;
+    [SerializeField] private AudioClip GruntClip;
+    private Coroutine footstepRoutine;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); 
+        playerMove = FindFirstObjectByType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaySounds();
+    }
+    private void PlaySFX(AudioClip clip)
+    {
+        if (audioSource.clip != clip || !audioSource.isPlaying)
+        {
+            audioSource.Stop();
+            audioSource.clip = clip;
+        }
+        audioSource.Play();
+    }
+
+    private IEnumerator FootstepCoroutine()
+    {
+        while (true)
+        {
+            float interval = walkingInterval;
+            if (playerMove.isRunning)
+            {
+                interval *= runningMultiplier;
+            }
+            walkingTimer -= Time.deltaTime;
+            if (walkingTimer <= 0)
+            {
+                if (walkingClips != null && walkingClips.Count > 0)
+                {
+                    PlayOneShot(walkingClips[Random.Range(0, walkingClips.Count)]);
+                }
+            }
+            yield return new WaitForSeconds(interval + 1f);
+        }
+    }
+
+    private void PlayOneShot(AudioClip clip)
+    {
+        if (clip != null)
+            audioSource.PlayOneShot(clip);
+    }
+
+    private void PlaySounds()
+    {
+        if (playerMove.hasPlayed) return;
+        
+        if (playerMove.hasHardDropped && playerMove.IsGrounded)
+        {
+            PlaySFX(hardLandClip);
+            playerMove.hasPlayed = true;
+        }
+
+        if (!playerMove.isHoldingDash && playerMove.isDashing)
+        {
+            PlaySFX(dashClip);
+            playerMove.hasPlayed = true;
+           playerMove.Dashed = false;
+        }
+
+        if (playerMove.isKnockedBack)
+        {
+            PlaySFX(hurtClip);
+            playerMove.hasPlayed = true;
+        }
+
+        if (playerMove.Jumped == true)
+        {
+            OneShotSource.PlayOneShot(jumpClip);
+            OneShotSource.PlayOneShot(GruntClip);
+            playerMove.hasPlayed = true;
+           playerMove.Jumped = false;
+           
+        }
+        
+        if (playerMove.IsGrounded && playerMove.horizontalMovement != 0 && !playerMove.isDashing)
+        {
+            if (footstepRoutine == null)
+            {
+                footstepRoutine = StartCoroutine(FootstepCoroutine());
+            }
+        }
+        else
+        {
+            if (footstepRoutine != null)
+            {
+                StopCoroutine(footstepRoutine);
+                footstepRoutine = null;
+            }
+        }
+        
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Stop();
+            playerMove.hasPlayed = true;
+        }
+    }
+}
